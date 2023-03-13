d4256b01b838f6b686b299c486b85c4463df8ffa
Brahm V <brahmvanh@gmail.com>
2023-03-13 14:15:15 -0400

    First commit. Imported html from bootstrap studio without CSS. Created js file with element variables and empty functions for each button.

---
+++ script.js
@@ -0,0 +1,58 @@
+// Color theme button group elements
+const darkColorThemeBtn = document.getElementById('darkColorTheme');
+const lightColorThemeBtn = document.getElementById('lightColorTheme');
+const invertedColorThemeBtn = document.getElementById('invertedColorTheme');
+
+// Text size button group elements
+const smallerTextBtn = document.getElementById('smallerText');
+const mediumTextBtn = document.getElementById('mediumText');
+const largerTextBtn = document.getElementById('largerText');
+
+// Cursor options button elements
+const oversizeCursorBtn = document.getElementById('oversizeCursor');
+const readingMaskCursorBtn = document.getElementById('readingMaskCursor');
+const readingGuideCursorBtn = document.getElementById('readingGuideCursor');
+
+// Change text spacing toggle
+const textSpacingBtn = document.getElementById('textSpacing');
+
+// Change to a dyslexia-friendly font element
+const dyslexiaFriendlyFontBtn = document.getElementById('dyslexiaFriendlyFont');
+
+// Hide images element
+const hideImagesBtn = document.getElementById('hideImages');
+
+// Highlight links on page button
+const highlightLinksBtn = document.getElementById('highlightLinks');
+
+const renderDarkTheme = (event) => {};
+
+const renderLightTheme = (event) => {};
+
+const renderInvertedColorTheme = (event) => {};
+
+const renderSmallerText = (event) => {};
+
+const renderMediumText = (event) => {};
+
+const renderLargerText = (event) => {};
+
+const renderOversizedCursor = (event) => {};
+
+const renderReadingMaskCursor = (event) => {};
+
+const renderReadingGuideCursor = (event) => {};
+
+const renderIncreasedTextSpacing = (event) => {};
+
+const renderDecreasedTextSpacing = (event) => {};
+
+const renderDyslexiaFriendlyFont = (event) => {};
+
+const hideImages = (event) => {};
+
+const showImages = (event) => {};
+
+const highlightLinks = (event) => {};
+
+const unhighlightLinks = (event) => {};
